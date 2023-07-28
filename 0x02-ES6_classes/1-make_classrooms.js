47413126dc8bb5871df53fe17b4530f0763f41bb
Your Name <you@example.com>
2023-07-28 16:27:32 -0500

    backend

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const class1 = new ClassRoom(19);
-  const class2 = new ClassRoom(20);
-  const class3 = new ClassRoom(34);
-  const newList = [class1, class2, class3];
-  return (newList);
+  const cl1 = new ClassRoom(19);
+  const cl2 = new ClassRoom(20);
+  const cl3 = new ClassRoom(34);
+  const list = [cl1, cl2, cl3];
+  return (list);
 }
