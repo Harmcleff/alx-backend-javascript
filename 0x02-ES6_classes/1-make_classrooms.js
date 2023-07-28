888b7f872368b647b3da016ecc0f1182f505f638
Sanyaolu Hammed <33807817+Harmcleff@users.noreply.github.com>
2023-07-28 18:00:34 -0500

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,11 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms() {
-  const cl1 = new ClassRoom(19);
-  const cl2 = new ClassRoom(20);
-  const cl3 = new ClassRoom(34);
-  const list = [cl1, cl2, cl3];
-  return (list);
-}
+const initializeRooms = () => {
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  const rooms = [room1, room2, room3];
+  return rooms;
+};
+
+export default initializeRooms;
