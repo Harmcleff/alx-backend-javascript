86acb474c8f83073be2520d904ea05853963168e
Your Name <you@example.com>
2023-07-28 11:01:57 -0500

    backend

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-=======
-/* eslint-disable no-console */
->>>>>>> 56911e290a5fabe40caff8d295d67cd17230f973
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
 
-<<<<<<< HEAD
   return Promise.all([photo, user])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstname} ${res[1].lastname}`);
@@ -17,10 +12,3 @@ export default function handleProfileSignup() {
       console.log('Signup system offline');
     });
 }
-=======
-  return Promise.all([photo, user]).then((res) => {
-    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-  })
-    .catch(() => { console.log('Signup system offline'); });
-}
->>>>>>> 56911e290a5fabe40caff8d295d67cd17230f973
