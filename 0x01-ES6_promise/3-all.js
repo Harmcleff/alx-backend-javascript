31265c1c92f83d29a07537b3175e9f51fbc64acc
Sanyaolu Hammed <33807817+Harmcleff@users.noreply.github.com>
2023-07-28 11:06:53 -0500

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,17 @@
+/* eslint-disable camelcase */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
-
   return Promise.all([photo, user])
-    .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstname} ${res[1].lastname}`);
+    .then((results) => {
+      const new_res = results[0];
+      const { body } = new_res;
+      const res = results[1];
+      const first_name = res.firstName;
+      const last_name = res.lastName;
+      console.log(`${body} ${first_name} ${last_name}`);
     })
     .catch(() => {
       console.log('Signup system offline');
